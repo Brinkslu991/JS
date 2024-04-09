b97354801cf096ec371650a126167ed0fd27c9fb
Brinkslu991 <brinkslu991@northwested.org>
2024-04-09 11:32:44 -0400

    khvjdf

---
+++ template/main.js
@@ -0,0 +1,17 @@
+// Lucas Brinks
+
+/*
+This is 
+a 
+multiline
+JavaScript comment
+*/
+
+
+
+const numInches = 10;
+const conversionFactor = 2.54;
+const numCentimeters = numInches * conversionFactor;
+const myOutput = `${numInches} inches is equivalent to ${numCentimeters}
+centimeters.`;
+console.log(myOutput);
